dc72fb68f7b5e4ce113191bf7fb4c137b46c6bbb
YehiaElHadidi <hadidiyehia@gmail.com>
2022-12-19 21:18:18 +0200

    partition array fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,37 +8,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Partition> partition = new ArrayList<>();
-//        partition.add(new Partition(0, 90));
-//        partition.add(new Partition(1, 20));
-//        partition.add(new Partition(2, 5));
-//        partition.add(new Partition(3, 30));
-//        partition.add(new Partition(4, 120));
-//        partition.add(new Partition(5, 80));
+        partition.add(new Partition(0, 90));
+        partition.add(new Partition(1, 20));
+        partition.add(new Partition(2, 5));
+        partition.add(new Partition(3, 30));
+        partition.add(new Partition(4, 120));
+        partition.add(new Partition(5, 80));
         ArrayList<Process> process = new ArrayList<>();
-//        process.add(new Process("1", 15));
-//        process.add(new Process("2", 90));
-//        process.add(new Process("3", 30));
-//        process.add(new Process("4", 100));
-
-        System.out.println("Enter the Number of Partition: ");
-        int n = sc.nextInt(), partition_size;
-        for(int i = 0 ; i < n; i++){
-            System.out.println("Enter Partition Size: ");
-            partition_size = sc.nextInt();
-            partition.add(new Partition(i,partition_size));
-        }
-
-
-        System.out.println("Enter the Number of Process: ");
-        n = sc.nextInt();
-        String process_name;
-        int  process_size;
-        for (int i = 0; i < n; i++){
-            System.out.println("Process name and its size: ");
-            process_name = sc.next();
-            process_size = sc.nextInt();
-            process.add(new Process(process_name,process_size));
-        }
+        process.add(new Process("1", 15));
+        process.add(new Process("2", 90));
+        process.add(new Process("3", 30));
+        process.add(new Process("4", 100));
+        // --------- lw htd5l inputs tanya ------------
+//        System.out.println("Enter the Number of Partition: ");
+//        int n = sc.nextInt(), partition_size;
+//        for(int i = 0 ; i < n; i++){
+//            System.out.println("Enter Partition Size: ");
+//            partition_size = sc.nextInt();
+//            partition.add(new Partition(i,partition_size));
+//        }
+//
+//
+//        System.out.println("Enter the Number of Process: ");
+//        n = sc.nextInt();
+//        String process_name;
+//        int  process_size;
+//        for (int i = 0; i < n; i++){
+//            System.out.println("Process name and its size: ");
+//            process_name = sc.next();
+//            process_size = sc.nextInt();
+//            process.add(new Process(process_name,process_size));
+//        }
         System.out.println("""
                 Select the policy you want to apply:
                 1. First fit
@@ -47,18 +47,20 @@ public class Main {
                 4. exit"""
         );
         // use the same testcases on all the algorithms
-        // as in the assignment
+        // zy md el dr 3ayz fa el assignment
         System.out.println("\nSelect Policy: ");
         int choice = sc.nextInt();
 
         while(choice != 4) {
-            ArrayList<Partition> partitionCopy = new ArrayList<>(partition);
-            ArrayList<Process> processCopy = new ArrayList<>(process);
+            ArrayList<Partition> partitionCopy = new ArrayList<>();
+            for (int i = 0; i < partition.size(); i++){
+                partitionCopy.add(new Partition(partition.get(i).partitionName,partition.get(i).partitionSize));
+            }
             if (choice == 1) {
-                policyAlgo= new FirstFit(partitionCopy,processCopy);
+                policyAlgo= new FirstFit(partitionCopy,process);
             }
             else if(choice==2){
-                policyAlgo=new BestFit(partitionCopy,processCopy);
+                policyAlgo=new BestFit(partitionCopy,process);
             }
             policyAlgo.executePolicy();
             System.out.println("""
